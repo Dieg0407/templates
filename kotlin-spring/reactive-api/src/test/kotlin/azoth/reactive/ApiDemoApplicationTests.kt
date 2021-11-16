8861bfaaedd812c19841bcdf6fc3bd4c75ec1d77
Alejandro <diego.pastor-ext@promart.pe>
2021-11-15 19:45:08 -0500

    added reactive demo

---
+++ kotlin-spring/reactive-api/src/test/kotlin/azoth/reactive/ApiDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package azoth.reactive
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
