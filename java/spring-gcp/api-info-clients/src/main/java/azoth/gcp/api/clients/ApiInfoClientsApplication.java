2c01f8bc77d0ffe6e59dbbe230be4e4ec845c897
Alejandro <diego.pastor@tektonlabs.com>
2021-12-21 11:09:00 -0500

    added base for full demo of deployment, gke, logging

---
+++ java/spring-gcp/api-info-clients/src/main/java/azoth/gcp/api/clients/ApiInfoClientsApplication.java
@@ -0,0 +1,13 @@
+package azoth.gcp.api.clients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiInfoClientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiInfoClientsApplication.class, args);
+	}
+
+}
---
+++ java/spring-gcp/api-info-clients/src/test/java/azoth/gcp/api/clients/ApiInfoClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package azoth.gcp.api.clients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiInfoClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
